6c869cc6e53359fa4e4d142f65a1e52a2ee68fc2
DESKTOP-ID0U6NI\JohnTing <jot.johnting@gmail.com>
2022-08-07 20:33:30 +0800

    temp

--- core/src/mindustry/input/Binding.java
+++ core/src/mindustry/input/Binding.java
@@ -12,9 +12,9 @@ public enum Binding implements KeyBind{
     pan(KeyCode.mouseForward),
 
     boost(KeyCode.shiftLeft),
-    command_mode(KeyCode.shiftLeft),
+    command_mode(KeyCode.g),
     control(KeyCode.controlLeft),
-    respawn(KeyCode.v),
+    respawn(KeyCode.b),
     select(KeyCode.mouseLeft),
     deselect(KeyCode.mouseRight),
     break_block(KeyCode.mouseRight),
@@ -57,7 +57,7 @@ public enum Binding implements KeyBind{
     fullscreen(KeyCode.f11),
     pause(KeyCode.space),
     minimap(KeyCode.m),
-    research(KeyCode.b),
+    research(KeyCode.n),
     planet_map(KeyCode.n),
     block_info(KeyCode.f1),
     toggle_menus(KeyCode.c),
@@ -71,6 +71,11 @@ public enum Binding implements KeyBind{
     chat_scroll(new Axis(KeyCode.scroll)),
     chat_mode(KeyCode.tab),
     console(KeyCode.f8),
+
+    freecam(KeyCode.v),
+    slowcam(KeyCode.controlLeft),
+    hide_units(KeyCode.space),
+    rebuild_brush(KeyCode.r),
     ;
 
     private final KeybindValue defaultValue;
