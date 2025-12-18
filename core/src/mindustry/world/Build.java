42133538ce16c6281fb68e5c714deec6daad8e52
johnting <jot.johnting@gmail.com>
2025-12-18 22:05:07 +0800

    v154.2

--- core/src/mindustry/world/Build.java
+++ core/src/mindustry/world/Build.java
@@ -95,6 +95,7 @@ public class Build{
             tile.build.noSleep();
             Fx.rotateBlock.at(tile.build.x, tile.build.y, tile.build.block.size);
             Events.fire(new BuildRotateEvent(tile.build, unit, previous));
+            if(!headless) Sounds.blockRotate.at(tile.build, 1f + Mathf.range(0.1f), 1f);
             return;
         }
 
@@ -116,7 +117,7 @@ public class Build{
             if(fogControl.isVisibleTile(team, tile.x, tile.y)){
                 result.placeEffect.at(tile.drawx(), tile.drawy(), result.size);
                 Fx.rotateBlock.at(tile.build.x, tile.build.y, tile.build.block.size);
-                //doesn't play a sound
+                ConstructBlock.playRepairSound(team, tile);
             }
 
             Events.fire(new BlockBuildEndEvent(tile, unit, team, false, tile.build.config()));
