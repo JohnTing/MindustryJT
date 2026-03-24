80a511fe889bdab65c3ce9d8e01039cbb9d5ac49
johnting <jot.johnting@gmail.com>
2026-03-24 21:01:59 +0800

    調整 PowerNode 的連接邏輯，將對角放置鍵的檢查移至連接鏈接的邏輯中

--- core/src/mindustry/world/blocks/power/PowerNode.java
+++ core/src/mindustry/world/blocks/power/PowerNode.java
@@ -211,10 +211,6 @@ public class PowerNode extends PowerBlock{
     protected void getPotentialLinks(Tile tile, Team team, Cons<Building> others){
         if(!autolink) return;
 
-        if (Core.input.keyDown(Binding.diagonalPlacement)) {
-            getPotentialLinksFull(tile, team, others);
-        }
-
         Boolf<Building> valid = other -> other != null && other.tile != tile && other.block.connectedPower && other.power != null &&
             (other.block.outputsPower || other.block.consumesPower || other.block instanceof PowerNode) &&
             overlaps(tile.x * tilesize + offset, tile.y * tilesize + offset, other.tile, laserRange * tilesize) && other.team == team &&
@@ -515,11 +511,21 @@ public class PowerNode extends PowerBlock{
             if(this == other){ //double tapped
                 if(other.power.links.size == 0){ //find links
                     Seq<Point2> points = new Seq<>();
-                    getPotentialLinks(tile, team, link -> {
-                        if(!insulated(this, link) && points.size < maxNodes){
-                            points.add(new Point2(link.tileX() - tile.x, link.tileY() - tile.y));
-                        }
-                    });
+
+                    if (Core.input.keyDown(Binding.diagonalPlacement)) {
+                        getPotentialLinksFull(tile, team, link -> {
+                            if(!insulated(this, link) && points.size < maxNodes){
+                                points.add(new Point2(link.tileX() - tile.x, link.tileY() - tile.y));
+                            }
+                        });
+                    } else {
+                        getPotentialLinks(tile, team, link -> {
+                            if(!insulated(this, link) && points.size < maxNodes){
+                                points.add(new Point2(link.tileX() - tile.x, link.tileY() - tile.y));
+                            }
+                        });
+                    }
+
                     configure(points.toArray(Point2.class));
                 }else{ //clear links
                     configure(new Point2[0]);
