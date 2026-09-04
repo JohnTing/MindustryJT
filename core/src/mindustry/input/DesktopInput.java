19e2a9fcf1d0837b874bdc06d492fd46d2feac29
DESKTOP-ID0U6NI\JohnTing <jot.johnting@gmail.com>
2026-09-04 22:51:54 +0800

    隱藏模式下可進入可控制的建築

--- core/src/mindustry/input/DesktopInput.java
+++ core/src/mindustry/input/DesktopInput.java
@@ -426,7 +426,7 @@ public class DesktopInput extends InputHandler{
             if(Core.input.keyDown(Binding.control) && Core.input.keyTap(Binding.select)){
                 Unit on = selectedUnit();
                 var build = selectedControlBuild();
-                if(on != null && !hiddenMode){
+                if(on != null && (!hiddenMode || on instanceof mindustry.gen.BlockUnitc)){
                     Call.unitControl(player, on);
                     shouldShoot = false;
                     recentRespawnTimer = 1f;
